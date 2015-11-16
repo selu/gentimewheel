e0821244e3578ccc4e9e549e2aabca09443d9d8a
Szabolcs Seláf <selu@selu.org>
2015-11-16 16:13:47 +0100

    add base_rotation for time zones

--- lib/gentimewheel/zone.rb
+++ lib/gentimewheel/zone.rb
@@ -4,14 +4,19 @@ module GenTimeWheel
 
     def initialize(zone)
       @tz = TZInfo::Timezone.get(zone)
+      @current_offset = @tz.period_for_utc(Time.now.utc).utc_total_offset
       @@zones << self
+    end
+
+    def base_rotate
+      @current_offset/240.0        # 360/86400
     end
 
     def generate(svg, radius)
       svg.g(stroke:"green", "stroke-width":3) do |g|
         g.circle(r:radius, fill:"white")
         24.times do |hour|
-          g.g(transform:"rotate(#{15*hour}) translate(0,#{-radius})") do |gh|
+          g.g(transform:"rotate(#{15*hour-base_rotate}) translate(0,#{-radius})") do |gh|
             gh.line(y2: 10)
             gh.text(hour, y:24, fill:"green", stroke:"none", "text-anchor":"middle")
           end
